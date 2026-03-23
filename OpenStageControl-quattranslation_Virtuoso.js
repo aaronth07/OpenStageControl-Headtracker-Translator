10ddbdd235287354278aa8e34b9246ecc746990b
aaronth07 <22606764+aaronth07@users.noreply.github.com>
2026-03-23 00:42:55 -0500

    Add files via upload

---
+++ OpenStageControl-quattranslation_Virtuoso.js
@@ -0,0 +1,41 @@
+let q = { x: 0, y: 0, z: 0, w: 1 };
+
+function sendQuat() {
+    const out = [
+        { type: "f", value: q.w },      
+        { type: "f", value: -q.x },      // roll
+        { type: "f", value: q.z },      // pitch  
+        { type: "f", value: -q.y }       // yaw
+    ];
+    // console.log("SEND /Virtuoso/quat", out); // uncomment to debug
+    send("10.0.0.106", 3001, "/Virtuoso/quat", ...out);
+}
+
+module.exports = {
+    init() {
+        console.log("INIT: quaternion module loaded");
+    },
+
+    oscInFilter(data) {
+        const { address, args } = data;
+
+        if (address.endsWith("/rotation/x")) {
+            q.z = args[0].value;
+            sendQuat();
+        }
+        else if (address.endsWith("/rotation/y")) {
+            q.y = args[0].value;
+            sendQuat();
+        }
+        else if (address.endsWith("/rotation/z")) {
+            q.x = args[0].value;
+            sendQuat();
+        }
+        else if (address.endsWith("/rotation/w")) {
+            q.w = args[0].value;
+            sendQuat();
+        }
+
+        return data; // allow normal processing
+    }
+};
